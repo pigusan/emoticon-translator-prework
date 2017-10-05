d852bf6618f3138d0a3aaa19423bd0c6d0d1ddc1
Brendan McIlhenny <bmcilhen@gmail.com>
2017-10-05 20:39:42 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,8 +14,8 @@ def load_library(file_path)
     if new_hash.key?('get_meaning') == false
       new_hash['get_meaning']= {}
       new_hash['get_emoticon'] = {}
-      new_hash['get_meaning'] = new_hash['get_meaning'].merge("#{japanese_emoticon}": label)
-      new_hash['get_emoticon'] = new_hash['get_emoticon'].merge("#{american_emoticon}": japanese_emoticon)
+      new_hash['get_meaning'] = new_hash['get_meaning'].merge("#{japanese_emoticon}" => label)
+      new_hash['get_emoticon'] = new_hash['get_emoticon'].merge("#{american_emoticon}"=> japanese_emoticon)
     else
       new_hash['get_meaning'][japanese_emoticon] = label
       new_hash['get_emoticon'][american_emoticon] = japanese_emoticon
