b50a8e4d051eb039d5f7ad7e97461468ac703977
Brendan McIlhenny <bmcilhen@gmail.com>
2017-10-05 20:51:20 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,7 @@ def get_japanese_emoticon(file_path, emoticon)
   if emoticon_hash['get_emoticon'][emoticon]
     emoticon_hash['get_emoticon'][emoticon]
   else
-    puts "Sorry, that emoticon was not found"
+    "Sorry, that emoticon was not found"
   end
 end
 
@@ -38,6 +38,6 @@ def get_english_meaning(file_path, emoticon)
   if emoticon_hash['get_meaning'][emoticon]
     emoticon_hash['get_emoticon'][emoticon]
   else
-    puts "Sorry, that emoticon was not found"
+    "Sorry, that emoticon was not found"
   end
 end
