d585bec3dfb0b04ac8fde844d61e0ce90ed95a9f
Brendan McIlhenny <bmcilhen@gmail.com>
2017-10-05 21:23:37 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -35,9 +35,10 @@ end
 
 def get_english_meaning(file_path, emoticon)
   emoticon_hash = load_library(file_path)
+  emoticon_hash['get_emoticon'][emoticon]
   #if emoticon_hash['get_meaning'][emoticon] == nil
     #{}"Sorry, that emoticon was not found"
   #else
-    emoticon_hash['get_emoticon'][emoticon]
+  
   #end
 end
